5de74e1b537d08b8a6414600e5b88542fd115013
Alice <alicexenawong@gmail.com>
2020-01-29 18:09:34 -0800

    Set up theme. Add to home page.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,86 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- Main -->
+          <div id="main">
+
+          <!-- Introduction -->
+            <section id="intro" class="main">
+              <div class="spotlight">
+                <div class="content">
+                  <header class="major">
+                    <h2>Introduction</h2>
+                  </header>
+                  <p>I've always loved making stuff, from websites to fantasy and sci-fi stories to cosplays based on games an shows. Even as a kid, I loved to tell stories and build robots. My early interest in robots led to an interest in web design. Whether for an end user, client, or reader I enjoy creating content of all sorts. </p>
+                  <ul class="actions">
+                    <li><a href="generic.html" class="button">Learn More</a></li>
+                  </ul>
+                </div>
+                <span class="image"><img src="images/Self photo.jpg" alt="" /></span>
+              </div>
+            </section>
+
+          <!-- First Section -->
+            <section id="developer" class="main">
+              <div class="spotlight">
+                <div class="content">
+                  <header class="major">
+                    <h2>Web Developer</h2>
+                  </header>
+                  <p>I could say I had an interest in coding from an early age. I built robots as a kid and one major part of making a robot was programming it. Back then it was a matter of drag-and-drop code blocks, but it gave me a taste of the fundamentals. Fast forward to college and I took my first coding class, and enjoyed seeing what I could make with the technology I had available. Fast forward to post-graduation, I decided to attend Actualize Coding Bootcamp and learned a comprehensive tech stack including Ruby, Rails, SQL, Javascript, Vue.js, CSS, HTML, among other things. I am currenly learning Python and Django. I enjoy seeing websites come to life and thinking about how to structure data and write logic to achieve the design goals.</p>
+                  <ul class="actions">
+                    <li><a href="generic.html" class="button">Learn More</a></li>
+                  </ul>
+                </div>
+                <!-- <span class="image"><img src="images/Self photo.jpg" alt="" /></span> -->
+              </div>
+            </section>
+
+          <!-- Second Section -->
+            <section id="writer" class="main">
+              <div class="spotlight">
+                <div class="content">
+                  <header class="major">
+                    <h2>Fiction Writer</h2>
+                  </header>
+                  <p>I've always loved telling stories. Even as a kid, I was making up characters and coming up with adventures for them to go on. I started to write actual prose starting in high school, though I honed my skill during my college years and continue to refine them post-graduation. I perfer to write stories with female leads in the genres of science-fiction and fantasy, though often it ends up being a combination of both those genres. I have written a number of novels, and hope to publish them eventually.</p>
+                  <ul class="actions">
+                    <li><a href="generic.html" class="button">Learn More</a></li>
+                  </ul>
+                </div>
+                <!-- <span class="image"><img src="images/Self photo.jpg" alt="" /></span> -->
+              </div>
+            </section>
+
+            <section id="cosplayer" class="main">
+              <div class="spotlight">
+                <div class="content">
+                  <header class="major">
+                    <h2>Cosplayer</h2>
+                  </header>
+                  <p>I've always liked the idea of dressing up as someone else, and I think my love of cosplay comes from not being able to dress up as much as I wanted to as a kid. Cosplay involves dressing up as a character, either from some form of media (games, anime, shows, movies, books) or an original character from the cosplayer's own imagination. Some cosplayers, myself included make our own costumes, which requires skill in sewing, patterning, and crafting props and armor among other things. It is an enjoyable hobby that lets me socialize with likeminded fans of series I like and to show off my handiwork.</p>
+                  <ul class="actions">
+                    <li><a href="generic.html" class="button">Learn More</a></li>
+                  </ul>
+                </div>
+                <!-- <span class="image"><img src="images/Self photo.jpg" alt="" /></span> -->
+              </div>
+            </section>
+
+        </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style>
+</style>
 
+<script>
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!"
+    };
+  },
+  created: function() {},
+  methods: {}
+};
 </script>
